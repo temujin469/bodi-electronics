16e3220c759889c842fa11619282b3fbaf52e93d
uuganbayar <d.uuganbayar.stu@gmail.com>
2022-08-04 17:41:49 +0800

    tvnsh huudsiig hiile

---
+++ customJs/tvnsh.js
@@ -0,0 +1,14 @@
+function openTab(evt, contentId) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("box");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(contentId).style.display = "block";
+  let current = evt.currentTarget;
+  current.classList.toggle("active");
+}
