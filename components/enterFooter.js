9f20eacd9de648fbd29f4637012475994908e714
uuganbayar <d.uuganbayar.stu@gmail.com>
2022-08-09 14:52:09 +0800

    zarim neg zvil zaslaa

--- components/enterFooter.js
+++ components/enterFooter.js
@@ -1,35 +1,35 @@
-const footerSection = document.getElementById('footer-section');
+const footerSection = document.getElementById("footer-section");
 
 const footer = `
 <!-- footer start -->
 <footer id="footer" class="border-top-0 mt-0">
-  <div class="container-fluid py-4 px-md-5">
-    <div class="row py-md-5 gap-4 gap-lg-0 p-0">
-      <div class="col-lg-6 col-xl-3 d-flex align-items-center p-0">
-      <a href="/entera/index.html">
-        <img class="logo" src="../logo/entera logo.png" />
+<div class="container-fluid py-4 px-md-5">
+  <div class="row py-md-5 p-0">
+    <div class="col-lg-6 col-xl-3 d-flex align-items-center p-0">
+      <a href="/index.html">
+        <img class="logo" src="/logo/entera logo.png" />
       </a>
-      </div>
-      <div class="col-lg-6 col-xl-3 footer-text p-0">
-        <i class="fa-solid fa-phone"></i>
-        <div>
-          <h5 class="m-0 p-0">(976) 70108010</h5>
-          <h5 class="m-0 p-0">(976) 75855577</h5>
-        </div>
-      </div>
-      <div class="col-lg-6 col-xl-3 footer-text p-0">
-        <i class="fa-solid fa-envelope"></i>
-        <h5>info@bodi-electronics.com</h5>
-      </div>
-      <div class="col-lg-6 col-xl-3 footer-text p-0">
-        <div><i class="fa-solid fa-location-dot"></i></div>
-        <h5>
-          Чингисийн өргөн чөлөө-24, 1-р хороо, Парк Плэйс, 901 тоот,
-          Сүхбаатар дүүрэг Улаанбаатар 14241, Монгол улс
-        </h5>
+    </div>
+    <div class="col-lg-6 col-xl-3 footer-text p-0">
+      <i class="fa-solid fa-phone"></i>
+      <div>
+        <h5 class="m-0 p-0">(976) 70108010</h5>
+        <h5 class="m-0 p-0">(976) 75855577</h5>
       </div>
     </div>
+    <div class="col-lg-6 col-xl-3 footer-text ps-0 pe-xl-5">
+      <div><i class="fa-solid fa-envelope"></i></div>
+      <h5 class="text-nowrap">info@bodi-electronics.com</h5>
+    </div>
+    <div class="col-lg-6 col-xl-3 footer-text p-0">
+      <div><i class="fa-solid fa-location-dot"></i></div>
+      <h5>
+        Чингисийн өргөн чөлөө-24, 1-р хороо, Парк Плэйс, 901 тоот,
+        Сүхбаатар дүүрэг Улаанбаатар 14241, Монгол улс
+      </h5>
+    </div>
   </div>
+</div>
   <div class="container d-flex justify-content-lg-center flex-column">
     <div
       class="pb-4 pb-md-5 social-links d-flex flex-row gap-md-5 justify-content-between justify-content-lg-center w-100">
